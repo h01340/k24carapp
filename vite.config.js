c13eee06698d7c4c92b3592435f248db5b5950fe
h01340 <minna.pellikka@haaga-helia.fi>
2024-04-16 10:06:03 +0300

    homepage correction

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/reactapp/',
-  plugins: [react()]
+  plugins: [react()],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+  }
 })
