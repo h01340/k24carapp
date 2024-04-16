0d728921fa970d3af8c3eb01e4f7abab3e08f017
h01340 <minna.pellikka@haaga-helia.fi>
2024-04-16 11:36:09 +0300

    name changed

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/carapp/',
+  base: '/k24carapp/',
   plugins: [react()],
   test: {
     globals: true,
