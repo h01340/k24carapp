c6cb60e3f8ee0b1c2bed7f21b7d797609dabb050
h01340 <minna.pellikka@haaga-helia.fi>
2024-04-16 11:28:11 +0300

    minor changes

--- src/components/EditCar.jsx
+++ src/components/EditCar.jsx
@@ -5,8 +5,9 @@ import TextField from '@mui/material/TextField';
 import Dialog from '@mui/material/Dialog';
 import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
-
 import DialogTitle from '@mui/material/DialogTitle';
+
+
 export default function EditCar(props) {
 
   const [car, setCar] = React.useState({
